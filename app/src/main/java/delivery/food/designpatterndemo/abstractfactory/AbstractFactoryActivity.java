85badb9d3ffac6b5985ec1fc6987aba13ab0ddc4
Ratanak Pek <ratanakpek088@gmail.com>
2018-03-28 06:18:56 +0700

    first init 2

--- app/src/main/java/delivery/food/designpatterndemo/abstractfactory/AbstractFactoryActivity.java
+++ app/src/main/java/delivery/food/designpatterndemo/abstractfactory/AbstractFactoryActivity.java
@@ -2,7 +2,10 @@ package delivery.food.designpatterndemo.abstractfactory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import delivery.food.designpatterndemo.R;
 
@@ -13,18 +16,31 @@ public class AbstractFactoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abstract_factory);
 
-        AbstractFactory bread= FactoryGenerator.getFactory("BREAD");
-        Bread br= bread.getBread("BRI");
-        Log.e("rtk", br.name()+" -> "+ br.calories());
+        Toolbar tb = findViewById(R.id.toolbar);
+        if (tb != null) {
+            setSupportActionBar(tb);
+            setTitle("");
+        }
+
+        AbstractFactory bread = FactoryGenerator.getFactory("BREAD");
+        Bread br = bread.getBread("BRI");
+        Log.e("rtk", br.name() + " -> " + br.calories());
 
 
         AbstractFactory filling = FactoryGenerator.getFactory("FILLING");
         Filling fi = filling.getFilling("CH");
-        Log.e("rtk", fi.name()+" -> "+ fi.calories());
+        Log.e("rtk", fi.name() + " -> " + fi.calories());
 
         AbstractFactory drink = FactoryGenerator.getFactory("DRINK");
         Drink dri = drink.getDrink("DRI");
-        Log.e("rtk", dri.name()+" -> "+ dri.calories());
+        Log.e("rtk", dri.name() + " -> " + dri.calories());
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_item, menu);
+        return true;
     }
 }
